8599d73d263621a1fbb93546b89977b4e4172809
koenhoeymans <koenhoeymans@gmail.com>
2017-12-03 22:00:20 +0100

    remove unnecessary phpdoc

--- src/Comment.php
+++ src/Comment.php
@@ -6,15 +6,11 @@ interface Comment extends Component
 {
     /**
      * The text of the comment.
-     *
-     * @return string
      */
     public function getValue() : string;
 
     /**
      * Sets the text of the comment.
-     *
-     * @param string $value
      */
     public function setValue(string $value) : void;
 }
